b65c0809c29e8d72f1e7b3b96d4de07bf9aa7d97
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-25 07:04:54 -0600

    add workout create method in controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -9,6 +9,11 @@ class WorkoutsController<ApplicationController
   end
 
   def create
-    
+    @workout = current_user.workouts.new(workout_params)
+    if @workout.save
+      redirect_to user_workout_path(current_user, @workout)
+    else
+      render :new
+    end
   end
 end
