ad032875b5222fa1214519be4e3b0819068427d1
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-26 15:16:24 -0600

    admin can delete user

---
+++ spec/features/user/admin_can_delete_user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "As a user" do
+  before(:all) do
+    reset_session!
+  end
+  
+  it "admin can see all users" do 
+    admin = create(:user, role: 1) 
+    user1 = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    
+    visit admin_users_path
+    expect(User.count).to eq(2)
+    page.all("#delete")[1].click_link("Delete")
+    expect(User.count).to eq(1)
+    expect(page).to_not have_content(user1.email)
+  end
+end
