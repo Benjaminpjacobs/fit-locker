59d0c32910242b73c49a8170731ccd648017f6da
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-26 15:32:51 -0600

    lift set before actions

--- app/controllers/lift_sets_controller.rb
+++ app/controllers/lift_sets_controller.rb
@@ -1,46 +1,38 @@
 class LiftSetsController < ApplicationController
+  before_action :set_workout
+  before_action :set_strength_exercise
+  before_action :set_lift_set, only: [:edit, :update, :destroy]
+  
   def new
-    @workout  = Workout.find(params[:workout_id])
-    @exercise  = StrengthExercise.find(params[:strength_exercise_id])
     @lift_set = LiftSet.new
   end
 
   def create
-    strength_exercise = StrengthExercise.find(params[:strength_exercise_id])
-    lift_set = strength_exercise.lift_sets.new(lift_set_params)
+    lift_set = @exercise.lift_sets.new(lift_set_params)
     if lift_set.save
       flash[:success] = "Set Added"
-      redirect_to user_workout_strength_exercise_path(current_user, strength_exercise.workout, strength_exercise)
+      redirect_to user_workout_strength_exercise_path(current_user, @workout, @exercise)
     else
       render :new
     end
   end
   
   def edit
-    @workout  = Workout.find(params[:workout_id])
-    @exercise  = StrengthExercise.find(params[:strength_exercise_id])
-    @lift_set = LiftSet.find(params[:id])
   end
 
   def update
-    workout = Workout.find(params[:workout_id])
-    exercise  = StrengthExercise.find(params[:strength_exercise_id])
-    lift_set = LiftSet.find(params[:id])
-    lift_set.update(lift_set_params)
-    if lift_set.save
+    @lift_set.update(lift_set_params)
+    if @lift_set.save
       flash[:success] = "Exercise updated!"
-      redirect_to user_workout_strength_exercise_path(current_user, exercise.workout, exercise)
+      redirect_to user_workout_strength_exercise_path(current_user, @workout, @exercise)
     else
       render :edit
     end
   end
 
   def destroy
-    workout = Workout.find(params[:workout_id])
-    exercise = StrengthExercise.find(params[:strength_exercise_id])
-    lift_set = LiftSet.find(params[:id])
-    lift_set.destroy
-    redirect_to user_workout_strength_exercise_path(current_user, exercise.workout, exercise)
+    @lift_set.destroy
+    redirect_to user_workout_strength_exercise_path(current_user, @workout, @exercise)
   end
 
   private
@@ -49,4 +41,15 @@ class LiftSetsController < ApplicationController
       params.require(:lift_set).permit(:weight, :reps)
     end
     
+    def set_workout
+      @workout = Workout.find(params[:workout_id])
+    end
+
+    def set_lift_set
+      @lift_set = LiftSet.find(params[:id])
+    end
+
+    def set_strength_exercise
+      @exercise = StrengthExercise.find(params[:strength_exercise_id])
+    end
 end
